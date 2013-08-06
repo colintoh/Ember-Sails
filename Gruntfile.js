6b99547c6f0d4df931128a04944e50748bdb24e3
Colin Toh <colinnus@gmail.com>
2013-08-07 00:24:27 +0800

    - Clean up grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,4 @@
-/**
- * Gruntfile
- *
- * If you created your Sails app with `sails new foo --linker`,
- * the following files will be automatically injected (in order)
- * into the EJS and HTML files in your `views` and `assets` folders.
- *
- * At the top part of this file, you'll find a few of the most commonly
- * configured options, but Sails' integration with Grunt is also fully
- * customizable.  If you'd like to work with your assets differently
- * you can change this file to do anything you like!
- *
- * More information on using Grunt to work with static assets:
- * http://gruntjs.com/configuring-tasks
- */
-
 module.exports = function (grunt) {
-
-
-
-  /**
-   * CSS files to inject in order
-   * (uses Grunt-style wildcard/glob/splat expressions)
-   *
-   * By default, Sails also supports LESS in development and production.
-   * To use SASS/SCSS, Stylus, etc., edit the `sails-linker:devStyles` task
-   * below for more options.  For this to work, you may need to install new
-   * dependencies, e.g. `npm install grunt-contrib-sass`
-   */
 
   var vendorCss = [
     'vendor/styles/bootstrap.css'
@@ -37,36 +9,6 @@ module.exports = function (grunt) {
   ]
 
   var cssFilesToInject = vendorCss.concat(userCss);
-
-
-  /**
-   * Javascript files to inject in order
-   * (uses Grunt-style wildcard/glob/splat expressions)
-   *
-   * To use client-side CoffeeScript, TypeScript, etc., edit the
-   * `sails-linker:devJs` task below for more options.
-   */
-
-  // var jsFilesToInject = [
-
-  //   // Below, as a demonstration, you'll see the built-in dependencies
-  //   // linked in the proper order order
-
-  //   // Bring in the socket.io client
-  //   'linker/js/socket.io.js',
-
-  //   // then beef it up with some convenience logic for talking to Sails.js
-  //   'linker/js/sails.io.js',
-
-  //   // A simpler boilerplate library for getting you up and running w/ an
-  //   // automatic listener for incoming messages from Socket.io.
-  //   'linker/js/app.js',
-
-  //   // *->    put other dependencies here   <-*
-
-  //   // All of the rest of your app scripts imported here
-  //   'linker/**/*.js'
-  // ];
 
 
   var vendorJs = [
@@ -139,6 +81,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-ember-templates');
 
+
+
+
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -170,42 +116,11 @@ module.exports = function (grunt) {
           dest: '.tmp/public'
         }
         ]
-      },
-      build: {
-        files: [
-          {
-          expand: true,
-          cwd: '.tmp/public',
-          src: ['**/*'],
-          dest: 'www'
-        }
-        ]
       }
     },
 
     clean: {
-      dev: ['.tmp/public/**'],
-      build: ['www']
-    },
-
-    less: {
-      dev: {
-        files: [
-          {
-          expand: true,
-          cwd: 'assets/styles/',
-          src: ['*.less'],
-          dest: '.tmp/public/styles/',
-          ext: '.css'
-        }, {
-          expand: true,
-          cwd: 'assets/linker/styles/',
-          src: ['*.less'],
-          dest: '.tmp/public/linker/styles/',
-          ext: '.css'
-        }
-        ]
-      }
+      dev: ['.tmp/public/**']
     },
 
     concat: {
@@ -322,7 +237,7 @@ module.exports = function (grunt) {
     'emberTemplates',
     'browserify',
     'clean:dev',
-    'less:dev',
+    // 'less:dev',
     'copy:dev'
   ]);
 
@@ -337,7 +252,7 @@ module.exports = function (grunt) {
   // When sails is lifted in production
   grunt.registerTask('prod', [
     'clean:dev',
-    'less:dev',
+    // 'less:dev',
     'copy:dev',
     'concat',
     'uglify',
@@ -346,19 +261,4 @@ module.exports = function (grunt) {
     'sails-linker:prodStyles'
   ]);
 
-  // When API files are changed:
-  // grunt.event.on('watch', function(action, filepath) {
-  //   grunt.log.writeln(filepath + ' has ' + action);
-
-  //   // Send a request to a development-only endpoint on the server
-  //   // which will reuptake the file that was changed.
-  //   var baseurl = grunt.option('baseurl');
-  //   var gruntSignalRoute = grunt.option('signalpath');
-  //   var url = baseurl + gruntSignalRoute + '?action=' + action + '&filepath=' + filepath;
-
-  //   require('http').get(url)
-  //   .on('error', function(e) {
-  //     console.error(filepath + ' has ' + action + ', but could not signal the Sails.js server: ' + e.message);
-  //   });
-  // });
 };
